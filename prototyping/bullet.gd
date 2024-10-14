ce8113f0747b232030a237baa8ca48fe6a3d91ef
Jason <141759463+jg-rockesheller@users.noreply.github.com>
2024-10-14 14:15:05 -0700

    fixed bullets killing enemies

--- prototyping/bullet.gd
+++ prototyping/bullet.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # TODO - fix enemy_prototype
 @export var enemy_prototype: Node2D
-var bullet_speed = 100
+var bullet_speed = 2
 var velocity = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -10,10 +10,10 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
+func _process(_delta: float):
 	position += position.direction_to(enemy_prototype.position) * bullet_speed
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("enemies"):
-		area.explode()
+	if area.is_in_group("enemy"):
+		enemy_prototype.explode()
 		queue_free()
